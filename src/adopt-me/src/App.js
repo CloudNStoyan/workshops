fa4ed891186a4836b6310a86a7c76984abaa851e
Stoyan <slownessbg@gmail.com>
2022-04-29 23:07:34 +0300

    following through 04-hooks
    adding QoL plugins and basic react state

--- src/adopt-me/src/App.js
+++ src/adopt-me/src/App.js
@@ -1,30 +1,13 @@
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
+import { render } from "react-dom";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Ricky",
-      animal: "Dog",
-      breed: "English Cocker Spaniel",
-    }),
-    React.createElement(Pet, {
-      name: "Pai",
-      animal: "Dog",
-      breed: "English Cocker Spaniel",
-    }),
-    React.createElement(Pet, {
-      name: "Kozunak",
-      animal: "Cat",
-      breed: "Yellow Common",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
