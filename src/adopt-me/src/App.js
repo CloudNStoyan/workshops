2e2db132b67ea6d43a2ccebd067bb55c918f4ff7
Stoyan <slownessbg@gmail.com>
2022-05-03 04:21:12 +0300

    finishing up complete intro to react v7

--- src/adopt-me/src/App.js
+++ src/adopt-me/src/App.js
@@ -1,19 +1,25 @@
 import { render } from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { StrictMode } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { StrictMode, useState } from "react";
+import ThemeContext from "./ThemeContext";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 
 const App = () => {
+  const theme = useState("darkblue");
   return (
     <StrictMode>
-      <BrowserRouter>
-        <h1>Adopt Me!</h1>
-        <Routes>
-          <Route path="/details/:id" element={<Details />} />
-          <Route path="/" element={<SearchParams />} />
-        </Routes>
-      </BrowserRouter>
+      <ThemeContext.Provider value={theme}>
+        <BrowserRouter>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeContext.Provider>
     </StrictMode>
   );
 };
