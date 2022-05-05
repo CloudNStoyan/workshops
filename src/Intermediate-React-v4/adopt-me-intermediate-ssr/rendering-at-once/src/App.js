85467a632cf1e23a612af053a9af4a53ea98c02e
Stoyan <slownessbg@gmail.com>
2022-05-05 17:23:33 +0300

    adding ssr exercises

---
+++ src/Intermediate-React-v4/adopt-me-intermediate-ssr/rendering-at-once/src/App.js
@@ -0,0 +1,24 @@
+import { Routes, Route, Link } from "react-router-dom";
+import { StrictMode, useState } from "react";
+import ThemeContext from "./ThemeContext";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+
+const App = () => {
+  const theme = useState("darkblue");
+  return (
+    <StrictMode>
+      <ThemeContext.Provider value={theme}>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </ThemeContext.Provider>
+    </StrictMode>
+  );
+};
+
+export default App;
