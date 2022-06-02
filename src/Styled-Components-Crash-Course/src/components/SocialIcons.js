bdf77a5c290638a19c43c4d701670d4d2f1b1d74
Stoyan Kolev <slownessbg@gmail.com>
2022-06-02 23:58:46 +0300

    adding styled component crash course project

---
+++ src/Styled-Components-Crash-Course/src/components/SocialIcons.js
@@ -0,0 +1,17 @@
+import {FaTwitter, FaFacebook,  FaLinkedin} from 'react-icons/fa';
+
+export default function SocialIcons() {
+    return (
+        <div>
+            <li>
+                <a href="#1"><FaTwitter /></a>
+            </li>
+            <li>
+                <a href="#2"><FaFacebook /></a>
+            </li>
+            <li>
+                <a href="#3"><FaLinkedin /></a>
+            </li>
+        </div>
+    )
+}
