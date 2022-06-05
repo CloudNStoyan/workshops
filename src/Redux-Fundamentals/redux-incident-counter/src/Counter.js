4dd84791673932ff2edfb608766d36b56e904394
Stoyan Kolev <slownessbg@gmail.com>
2022-06-05 17:38:55 +0300

    adding connecting state hooks to the store exercise

---
+++ src/Redux-Fundamentals/redux-incident-counter/src/Counter.js
@@ -0,0 +1,25 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { decrement, increment, set } from './actions';
+
+export const Counter = () => {
+  const incident = 'Incident';
+  const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+
+  const actions = bindActionCreators({ set, increment, decrement }, dispatch);
+
+  return (
+    <main className="Counter">
+      <h1>Days Since Last {incident}</h1>
+      <p className="count">{count}</p>
+      <section className="controls">
+        <button onClick={() => actions.increment()}>Increment</button>
+        <button onClick={() => actions.set(0)}>Reset</button>
+        <button onClick={() => actions.decrement()}>Decrement</button>
+      </section>
+    </main>
+  );
+};
+
+export default Counter;
