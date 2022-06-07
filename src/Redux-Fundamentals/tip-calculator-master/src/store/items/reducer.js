57d77e877a84c337b0240af41e887b7f11fdb206
Stoyan Kolev <slownessbg@gmail.com>
2022-06-07 23:22:04 +0300

    adding immer exercise solution

--- src/Redux-Fundamentals/tip-calculator-master/src/store/items/reducer.js
+++ src/Redux-Fundamentals/tip-calculator-master/src/store/items/reducer.js
@@ -1,3 +1,4 @@
+import produce from 'immer';
 import {
   ITEM_ADDED,
   ITEM_PRICE_UPDATED,
@@ -12,37 +13,27 @@ export const initialItems = [
   { uuid: id++, name: 'Vegan Ham Sandwich', price: 12, quantity: 1 }
 ];
 
-export const reducer = (state = initialItems, action) => {
+export const reducer = produce((state = initialItems, action) => {
   if (action.type === ITEM_ADDED) {
     const item = { uuid: id++, quantity: 1, ...action.payload };
-    return [...state, item];
+    state.push(item);
   }
 
   if (action.type === ITEM_REMOVED) {
-    return [...state.filter((item) => item.uuid !== action.payload.id)];
+    return state.filter((item) => item.uuid !== action.payload.id);
   }
 
   if (action.type === ITEM_PRICE_UPDATED) {
-    return state.map((item) => {
-      if (item.uuid === action.payload.uuid) {
-        return { ...item, price: action.payload.price };
-      }
-
-      return item;
-    });
+    const item = state.find((item) => item.uuid === action.payload.uuid);
+    item.price = parseInt(action.payload.price, 10);
   }
 
   if (action.type === ITEM_QUANTITY_UPDATED) {
-    return state.map((item) => {
-      if (item.uuid === action.payload.uuid) {
-        return { ...item, quantity: action.payload.quantity };
-      }
-
-      return item;
-    });
+    const item = state.find((item) => item.uuid === action.payload.uuid);
+    item.quantity = parseInt(action.payload.quantity, 10);
   }
 
   return state;
-};
+}, initialItems);
 
 export default reducer;
